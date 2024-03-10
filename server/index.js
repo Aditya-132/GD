c1f726f3d8bc97085c702ae1db2763b28989f765
Aditya Chincholkar <119576533+Aditya-132@users.noreply.github.com>
2024-03-10 21:37:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ mongoose.connect(`mongodb+srv://adityachincholkar11:${encodeURIComponent('Aditya
 // Setting up Socket.IO server
 const io = require('socket.io')(3001, {
     cors: {
-        origin: "gd-3swjvpexi-googledocs-projects.vercel.app",
+        origin: "https://gd-3gnq67424-googledocs-projects.vercel.app",
         methods: ['GET', 'POST']
     }
 });

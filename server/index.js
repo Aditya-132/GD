f73c6b6274a255a6d93455801c82db841b4618d6
Aditya Chincholkar <119576533+Aditya-132@users.noreply.github.com>
2024-03-10 20:55:27 +0530

    Update index.js
    change url

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ mongoose.connect(`mongodb+srv://adityachincholkar11:${encodeURIComponent('Aditya
 // Setting up Socket.IO server
 const io = require('socket.io')(3001, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://gd-snowy.vercel.app/",
         methods: ['GET', 'POST']
     }
 });
